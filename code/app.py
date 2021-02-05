1fa3bfbd184b6bb76472730dabd20434e8fbdf96
jason deadshot <neetikapaliwal19982016@gmail.com>
2021-02-05 19:25:22 +0530

    ..

--- code/app.py
+++ code/app.py
@@ -2,18 +2,39 @@ from sqlalchemy.orm import sessionmaker
 from python_orm import Visualizations
 from sqlalchemy import create_engine
 import streamlit as st
-
-engine =create_engine('sqlite:///project_db.sqlite3')
-Session =sessionmaker(bind=engine)
-sess =Session()
+import os
 
 
-data = sess.query(Visualizations).all()
-print(data)
+basepath = os.path.abspath(os.getcwd())
 
-for d in data:
-    st.title(d.name)
-    st.image('../figures/'+d.path)
+# engine =create_engine('sqlite:///project_db.sqlite3')
+# Session =sessionmaker(bind=engine)
+# sess =Session()
+
+def showWorldCloud():
+    st.header('Top 50 Words in News')
+    st.image('figures/wordcloud.png', use_column_width=True)
+    st.text("Here is the Description")
+
+st.title('News Analytica')
+st.sidebar.title('Options')
+viz = st.sidebar.button('Visualize')
+st.sidebar.button('View Dataset')
+select_value = st.selectbox("What would you like to Visualize?", options = ['Select', 'WordCloud', 'Most Occuring Words'])
+
+# data = sess.query(Visualizations).all()
+select_value = ''
+# if viz:
+#     select_value = st.selectbox("What would you like to Visualize?", options = ['Select', 'WordCloud', 'Most Occuring Words'])
+
+if select_value=="WordCloud":
+    st.header('Top 50 Words in News')
+    st.image('figures/wordcloud.png', use_column_width=True)
+    st.text("Here is the Description")
 
 
-    
+# for d in data:
+#     st.header(d.name)
+#     st.image('figures/'+d.path, use_column_width=True)
+
+
--- code/python_orm.py
+++ code/python_orm.py
@@ -26,7 +26,59 @@ if __name__=="__main__":
     Session =sessionmaker(bind=engine)
     session =Session()
     current_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    name
-    viz = Visualizations(path = 'figure3.png', date = current_time, name = "Visualization 1")
+
+    viz = Visualizations(path = 'wordcloud.png', date = current_time, name = "WORD CLOUD")
     session.add(viz)
     session.commit()
+    
+    viz = Visualizations(path = 'figure1.png', date = current_time, name = "MOST OCCURING WORD IN HEADING")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure2.png', date = current_time, name = "MOST OCCURING WORD IN TEXT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure3.png', date = current_time, name = "OVERALL SENTIMENT OF AUTHORS")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure4.png', date = current_time, name = "DATE WISE AUTHOR NEWS COUNT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure5.png', date = current_time, name = "OVERALL HEADING OF SENTIMENT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure6.png', date = current_time, name = "OVERALL TEXT OF SENTIMENT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure7.png', date = current_time, name = "WORD COUNT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure8.png', date = current_time, name = "MONTH WISE SENTIMENT OF HEADLINES")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure9.png', date = current_time, name = "MONTH WISE SENTIMENT OF TEXT")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure10.png', date = current_time, name = "AUTHOR WISE SENTIMENT OF HEADING(TOP 10)")
+    session.add(viz)
+    session.commit()
+    
+    viz = Visualizations(path = 'figure12.png', date = current_time, name = "AUTHOR WISE SENTIMENT OF TEXT(TOP 10)")
+    session.add(viz)
+    session.commit()
+    viz = Visualizations(path = 'figure13.png', date = current_time, name = "YEAR WISE SENTIMENT OF HEADLINES")
+    session.add(viz)
+    session.commit()
+    viz = Visualizations(path = 'figure14.png', date = current_time, name = "YEAR WISE SENTIMENT OF TEXT")
+    session.add(viz)
+    session.commit()
+    
+    
